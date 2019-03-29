06cd32ea9e1e9ca6958b4bf445afea6b693402da
How Youth <917879609@qq.com>
2019-03-29 08:28:27 +0800

    Quartz集成

--- demo03/src/main/java/com/hallth/utils/SpringQtz.java
+++ demo03/src/main/java/com/hallth/utils/SpringQtz.java
@@ -1,4 +1,24 @@
 package com.hallth.utils;
 
-public class SpringQtz {
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
+
+public class SpringQtz extends QuartzJobBean{
+    private static int counter = 0;
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        System.out.println();
+        long ms = System.currentTimeMillis();
+        System.out.println("\t\t" + new Date(ms));
+        System.out.println(ms);
+        System.out.println("(" + counter++ + ")");
+        String s = (String) context.getMergedJobDataMap().get("service");
+        System.out.println(s);
+        System.out.println();
+    }
+
 }
--- demo03/src/main/java/com/hallth/utils/SpringQtz2.java
+++ demo03/src/main/java/com/hallth/utils/SpringQtz2.java
@@ -1,4 +1,17 @@
 package com.hallth.utils;
 
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
+
 public class SpringQtz2 {
+    private static int counter = 0;
+    public void doSomething() {
+        System.err.println("****:" + System.currentTimeMillis());
+    }
+
 }
+
